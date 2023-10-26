46518f709a1fe065d2903487fa1f4194e1e00740
shaun mitchell <shaun@shitchell.com>
2023-10-26 08:39:48 -0400

    updates to variable output

--- examples/variables.sh
+++ examples/variables.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# This script demonstrates the various data types available to bash scripts.
+# This script demonstrates how to declare variables in bash.
 
 echo "# Variables #"
 
@@ -20,8 +20,30 @@ echo "organization = ${organization}"
 school="Georgia Gwinnet College"
 echo "school = ${school}"
 
+
 # 3. let <name>=<value> <name2>=<value2> ...
 #   This syntax ONLY works for integers but allows you to declare multiple
 #   variables at once and use math operators.
 let a=1 b=2 c="a + b"
 echo "a = ${a}, b = ${b}, c = ${c}"
+
+
+## Setting a variable to the output of a command
+#
+# You can set a variable to the output of a command by wrapping the command in
+# $(). This is called "command substitution".
+# Note that the command will be run when the variable is declared, not when it
+# is used.
+echo
+echo "## Command Substitution ##"
+
+# This will print the current date inside the string:
+echo "The current date is $(date)."
+
+# The following two lines will produce the same output by setting the variable
+# `cur_date` to the output of the `date` command.
+cur_date=$(date)
+echo "The current date is ${cur_date}."
+
+# See examples/data-types.sh for more information on the different data types
+# available to bash scripts.
